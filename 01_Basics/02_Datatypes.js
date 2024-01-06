d94b35dcabe124f9fac753f32faf39654df2e911
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-06 07:26:50 +0000

    datatypes

---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; //Treat all JS code as newer version
+
+//alert( 3 + 3 )  //we are using nodejs, not browser
+
+console.log(3 
+    +
+    3); // code readability should be high
+
+console.log("Ruchita");
+
+let name = "Ruchita"
+let age= 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Ruchita");
+console.log(typeof age);
+console.log(typeof null); //Output: object
+console.log(typeof undefined); //Output: Undefined
+
+
