755e253004397bbf0fbc9232d899c0697df32996
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-06 11:05:36 +0000

    conversion and comaprison

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,44 @@
+// let score = "33abc"
+// let score = null
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+ 
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1;false => 0
+
+
+let isLoggedIn = "Ruchita"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Ruchita" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************Operations***************** //
+
+let str1 = "Hello"
+let str2 = " Ruchita"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,22 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+//The reason is that an equality check == and comparison > < >=
+// <= work differently
+// Comparison convert null to a number, treating it as 0
+// That's why null>= 0 is true and null > 0 is false
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined < 0);
+
+// we avoid to do comparison with such type i.i null and undefined
+
+// === it strictly checks the datatype and the content
+
+console.log("2"===2);  
