be1a0825ff841416a4a589044b72428417eac926
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-07 10:05:26 +0000

    Nums ans dates

--- 01_Basics/07_NumsAndMath.js
+++ 01_Basics/07_NumsAndMath.js
@@ -9,11 +9,29 @@ const balance = new Number(100)
 
 // const otherNUmber = 1123.849948
 
-console.log(otherNUmber.toPrecision(3));//presion focus starts from first
+// console.log(otherNUmber.toPrecision(3));//presion focus starts from first
 
 const hundreds=1000000
 // console.log(hundreds.toLocaleString('en-IN'));
 
 //  ++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
-h
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4, 3, 8, 9));
+console.log(Math.max(4, 3, 8, 9));
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
 
