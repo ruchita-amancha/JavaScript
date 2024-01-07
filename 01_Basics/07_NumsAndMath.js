6f51f72c8a08d2a0a9b4a0eedfd6cdc65c27cb2d
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-07 12:44:58 +0530

    Add files via upload

---
+++ 01_Basics/07_NumsAndMath.js
@@ -0,0 +1,19 @@
+const score =400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const otherNUmber = 1123.849948
+
+console.log(otherNUmber.toPrecision(3));//presion focus starts from first
+
+const hundreds=1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+//  ++++++++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+
