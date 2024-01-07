b73b6fcb5ad45865f0f2c230cb39b5b1950d7548
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-07 15:16:26 +0000

    Arrays

---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,48 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+console.log(myArr[0]);
+
+//Array methods
+
+myArr.push(6)
+myArr.push(7)
+myArr.pop()
+
+myArr.unshift(8) //gets added at 0th index 
+// Output: [
+//     8, 0, 1, 2,
+//     3, 4, 5, 6
+//   ]
+
+myArr.shift()
+// output: [
+//   0, 1, 2, 3,
+//   4, 5, 6
+// ]
+
+// console.log(myArr.includes(7));
+// console.log(myArr.indexOf(3));
+
+const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+//slice and spice study about this good topic
+
+console.log("A ", myArr);
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B ",myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ",myArr);
+console.log(myn2);
+
+
