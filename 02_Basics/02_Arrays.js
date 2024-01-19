bcc1c9c1e74b2c53adeb7c9c522451ff4b4aaa3f
ruchita-amancha <155519788+ruchita-amancha@users.noreply.github.com>
2024-01-19 06:15:44 +0000

    arrays

--- 02_Basics/02_Arrays.js
+++ 02_Basics/02_Arrays.js
@@ -1,28 +1,48 @@
 const marval_heros = ["thor", "Ironman", "spiderman"]
 const dc_heros = ["superman", "flash", "batman"]
+let array1=[]
 
-// marval_heros.push(dc_heros)
+for (let index = 0; index < marval_heros.length; index++) {
+    array1[index].push(marval_heros[index]) 
+
+    
+}
+console.log(array1);
+
 // console.log(marval_heros[3][1]);
 
 // const all_heros=marval_heros.concat(dc_heros)
 // console.log(all_heros);
 
 const all_new_heros =[...marval_heros, ...dc_heros] //spread opeartion
-// console.log(all_new_heros);
+console.log(all_new_heros);
 
 const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
 
 
-console.log((Array.isArray("Ruchita")));
-console.log((Array.from("Ruchita")));
-console.log((Array.from({name: "Ruchita"}))); //interesting case
+// console.log((Array.isArray("Ruchita")));
+// console.log((Array.from("Ruchita")));
+// console.log((Array.from({name: "Ruchita"}))); //interesting case
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
+
+let key
+const array = [1, 2, 3];
+
+// Adding an element to the array using spread syntax
+for (let index = 0; index < array.length; index++) {
+    array[index]=array.push[key]
+    
+}
+
+// const newArray = [...array, 4];
+
+console.log(newArray);
